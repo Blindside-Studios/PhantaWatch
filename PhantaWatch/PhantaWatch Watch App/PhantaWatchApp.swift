39e701ddde6952768e43e1192dd8c67a7418d10e
Blindside Studios <nicolas.helbig@icloud.com>
2026-06-27 17:27:46 +0200

    Project creation

---
+++ PhantaWatch/PhantaWatch Watch App/PhantaWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhantaWatchApp.swift
+//  PhantaWatch Watch App
+//
+//  Created by Nicolas Helbig on 27.06.26.
+//
+
+import SwiftUI
+
+@main
+struct PhantaWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
